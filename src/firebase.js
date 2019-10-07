ad885bc68527e8c37ad41aa46c63f950a8ed0444
Luis Vilchez <luisdv93@gmail.com>
2019-10-06 22:17:39 -0400

    remove firebase api keys. Oopss

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = firebase.initializeApp({
-  apiKey: "AIzaSyCV4QripKpACKORV3Y7BM2L_ocNMRM770E",
-  authDomain: "todoist-a54fd.firebaseapp.com",
-  databaseURL: "https://todoist-a54fd.firebaseio.com",
-  projectId: "todoist-a54fd",
-  storageBucket: "todoist-a54fd.appspot.com",
-  messagingSenderId: "831888623649",
-  appId: "1:831888623649:web:928a0482817048a74cbb60"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 });
 
 export { firebaseConfig as firebase };
